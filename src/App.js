b56b4dcff13b442bf01ae7443e2a3d7a0d3e052c
Roel Tombozafy <rtombozafy96@gmail.com>
2022-01-24 18:16:00 +0300

    Adding keys

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function LakeList({ lakes }) {
   return (
     <div>
       {lakes.map((lake) => (
-        <>
+        <div key={lake.id}>
           <h2>{lake.name}</h2>
           <p>Accessed by: {lake.trailhead}</p>
-        </>
+        </div>
       ))}
     </div>
   );
