d6c5a10607af9adf48e30cee0a595ffe536da886
Roel Tombozafy <rtombozafy96@gmail.com>
2022-01-26 15:27:19 +0300

    Updating with the useEffect dependency array

--- src/App.js
+++ src/App.js
@@ -2,28 +2,36 @@ import { useState, useEffect } from "react";
 
 import "./App.css";
 
-function Checkbox() {
-  const [checked, setChecked] = useState(false);
-  useEffect(() => {
-    alert(`checked: ${checked.toString()}`);
-  });
-
-  return (
-    <>
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={() => setChecked((checked) => !checked)}
-      />
-      {checked ? "checked" : "not checked"}
-    </>
-  );
-}
-
 function App() {
+  const [val, setVal] = useState("");
+  const [val2, setVal2] = useState("");
+
+  useEffect(() => {
+    console.log(`field 1 ${val}`);
+  }, [val]);
+  useEffect(() => {
+    console.log(`field 2 ${val2}`);
+  }, [val2]);
+
   return (
     <div className="App">
-      <Checkbox />
+      <label>
+        Favorite Phrase:
+        <input
+          type="text"
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+      </label>{" "}
+      <br />
+      <label>
+        Second Favorite Phrase:
+        <input
+          type="text"
+          value={val2}
+          onChange={(e) => setVal2(e.target.value)}
+        />
+      </label>
     </div>
   );
 }
