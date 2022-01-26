4b36154297287602373ae943cb52b4a8403944e7
Roel Tombozafy <rtombozafy96@gmail.com>
2022-01-26 15:48:38 +0300

    Fetching data with useEffect

--- src/App.js
+++ src/App.js
@@ -2,38 +2,36 @@ import { useState, useEffect } from "react";
 
 import "./App.css";
 
-function App() {
-  const [val, setVal] = useState("");
-  const [val2, setVal2] = useState("");
-
-  useEffect(() => {
-    console.log(`field 1 ${val}`);
-  }, [val]);
-  useEffect(() => {
-    console.log(`field 2 ${val2}`);
-  }, [val2]);
-
+function UserItem({ name, mail }) {
   return (
-    <div className="App">
-      <label>
-        Favorite Phrase:
-        <input
-          type="text"
-          value={val}
-          onChange={(e) => setVal(e.target.value)}
-        />
-      </label>{" "}
-      <br />
-      <label>
-        Second Favorite Phrase:
-        <input
-          type="text"
-          value={val2}
-          onChange={(e) => setVal2(e.target.value)}
-        />
-      </label>
+    <div>
+      <h2>{name}</h2>
+      <p>{mail}</p>
     </div>
   );
+}
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/users`)
+      .then((res) => res.json())
+      .then(setUser)
+      .catch(console.error);
+  }, []);
+
+  if (user) {
+    return (
+      <div className="App">
+        {user.map((item, index) => (
+          <UserItem key={index} name={item.name} mail={item.email} />
+        ))}
+      </div>
+    );
+  }
+
+  return null;
 }
 
 export default App;
