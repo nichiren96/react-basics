af6cf204feaea9e7fe9eb38108fa2a9028c1123c
Roel Tombozafy <rtombozafy96@gmail.com>
2022-01-26 15:14:56 +0300

    Working with useEffect

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 
-function App() {
-  const [year, setYear] = useState(2022);
-  const [manager, setManager] = useState("Andy");
-  const [status, setStatus] = useState("Open");
+function Checkbox() {
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    alert(`checked: ${checked.toString()}`);
+  });
 
   return (
     <>
-      <div className="App">
-        <h1>{year}</h1>
-        <button onClick={() => setYear(year + 1)}>New Year</button>
-      </div>
-      <div className="App">
-        <h1>Manager on Duty: {manager}</h1>
-        <button onClick={() => setManager("Emily")}>New Manager</button>
-      </div>
-      <div className="App">
-        <h1>Status: {status}</h1>
-        <button onClick={() => setStatus("Open")}>Open</button>
-        <button onClick={() => setStatus("Back in 5")}>Break</button>
-        <button onClick={() => setStatus("Closed")}>Closed</button>
-      </div>
+      <input
+        type="checkbox"
+        value={checked}
+        onChange={() => setChecked((checked) => !checked)}
+      />
+      {checked ? "checked" : "not checked"}
     </>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Checkbox />
+    </div>
   );
 }
 
