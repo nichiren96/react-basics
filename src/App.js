45354bb99c7568eb80040bc599c5f9492a67a712
Roel Tombozafy <rtombozafy96@gmail.com>
2022-01-26 15:06:00 +0300

    Using multiple state variables

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [year, setYear] = useState(2022);
+  const [manager, setManager] = useState("Andy");
   const [status, setStatus] = useState("Open");
 
   return (
-    <div className="App">
-      <h1>Status: {status}</h1>
-      <button onClick={() => setStatus("Open")}>Open</button>
-      <button onClick={() => setStatus("Back in 5")}>Break</button>
-      <button onClick={() => setStatus("Closed")}>Closed</button>
-    </div>
+    <>
+      <div className="App">
+        <h1>{year}</h1>
+        <button onClick={() => setYear(year + 1)}>New Year</button>
+      </div>
+      <div className="App">
+        <h1>Manager on Duty: {manager}</h1>
+        <button onClick={() => setManager("Emily")}>New Manager</button>
+      </div>
+      <div className="App">
+        <h1>Status: {status}</h1>
+        <button onClick={() => setStatus("Open")}>Open</button>
+        <button onClick={() => setStatus("Back in 5")}>Break</button>
+        <button onClick={() => setStatus("Closed")}>Closed</button>
+      </div>
+    </>
   );
 }
 
